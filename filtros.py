93023ea3ff8ec8eeb80ee0df0def16c24574eae9
Max80318 <114278871+Max80318@users.noreply.github.com>
2026-05-28 02:19:36 +0000

    imagenes generadas

--- filtros.py
+++ filtros.py
@@ -5,28 +5,32 @@
 import cv2
 import numpy as np
 
-manzana = cv2.imread('C:/IA/manzana.jpg')
+manzana = cv2.imread('carro.jpg')
 
-cv2.imshow("Original", manzana)
+cv2.imwrite('original.jpg', manzana)
+
+#cv2.imshow("Original", manzana)
 
 kernel_3x3 = np.ones((3,3))/(3*3)
 
 output_3x3 = cv2.filter2D(manzana, -1, kernel_3x3)
 
-cv2.imshow('filtro de 3x3', output_3x3)
+cv2.imwrite('filtro3x3.jpg', output_3x3)
 
+#cv2.imshow('filtro de 3x3', output_3x3)
 
 kernel_11x11 = np.ones((11,11))/(11*11)
 
 output_11x11 = cv2.filter2D(manzana, -1, kernel_11x11)
 
-cv2.imshow('filtro de 3x3', output_11x11)
+cv2.imwrite('filtro11x11.jpg', output_11x11)
 
+#cv2.imshow('filtro de 11x11', output_11x11)
 
 kernel_31x31 = np.ones((31,31))/(31*31)
 
 output_31x31 = cv2.filter2D(manzana, -1, kernel_31x31)
 
-cv2.imshow('filtro de 3x3', output_31x31)
+cv2.imwrite('filtro31x31.jpg', output_31x31)
 
-
+#cv2.imshow('filtro de 31x31', output_31x31)
--- mascara binaria de imagenes.py
+++ mascara binaria de imagenes.py
@@ -8,7 +8,7 @@ Este es un archivo temporal.
 import cv2
 import numpy as np
 
-manzana = cv2.imread('C:/IA/manzana.jpg')
+manzana = cv2.imread('carro.jpg')
 
 print (manzana[0,0,:])
 
@@ -16,23 +16,33 @@ b = manzana[:,:,0]
 g = manzana[:,:,1]
 r = manzana[:,:,2]
 
-cv2.imshow('', manzana)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('original.jpg', manzana)
 
-cv2.imshow('', b)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('', manzana)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
-cv2.imshow('', r)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('canal_azul.jpg', b)
+
+#cv2.imshow('', b)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+cv2.imwrite('canal_rojo.jpg', r)
+
+#cv2.imshow('', r)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 img_gris = cv2.cvtColor(manzana, cv2.COLOR_BGR2GRAY)
+
+cv2.imwrite('gris.jpg', img_gris)
 
 binaria = np.uint8(255*(img_gris>155))
 
 binaria = np.uint8(255*(img_gris<200))
+
+cv2.imwrite('binaria.jpg', binaria)
 
 # cv2.imshow('', binaria)
 # cv2.waitKey(0)
@@ -43,12 +53,16 @@ binaria = np.uint8(255*(img_gris<200))
 gris_segmentada = img_gris*(binaria/255)
 gris_segmentada = np.uint8(img_gris*(binaria/255))
 
+cv2.imwrite('gris_segmentada.jpg', gris_segmentada)
+
 #segmentar por color
 seg_color = manzana.copy()
 
 seg_color[:,:,0] = np.uint8(b*(binaria/255)) 
 seg_color[:,:,1] = np.uint8(g*(binaria/255)) 
 seg_color[:,:,2] = np.uint8(r*(binaria/255)) 
+
+cv2.imwrite('segmentacion_color.jpg', seg_color)
 
 #cv2.imshow('', seg_color)
 #cv2.waitKey(0)
@@ -61,6 +75,7 @@ import matplotlib.pyplot as plt
 arreglo_img = img_gris.flatten()
 
 plt.hist(img_gris.flatten(), bins = 15)
+plt.savefig('histograma.png')
 plt.show()
 
 #binaria = np.uint8(255*(img_gris<139))
@@ -71,9 +86,4 @@ th_otsu,_ = cv2.threshold(img_gris, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 binaria_otsu = np.uint8(255*(img_gris<th_otsu))
 
-
-
-
-
-
-
+cv2.imwrite('binaria_otsu.jpg', binaria_otsu)
